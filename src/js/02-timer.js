4e9e747a883b5fde4d0ebf0d23ab945b43d0365d
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-01-07 20:37:53 +0200

    task_02_selectors

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+
+const refs = {
+    dataPicker: document.querySelector('#datetime-picker'),
+    startBtm: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]')
+}
+
+console.log(refs.seconds)
+
+
