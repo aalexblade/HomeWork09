4f97278f7b0dcb03b7bd1f5643b248468c64116f
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-01-09 20:39:44 +0200

    task_02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,10 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import 'notiflix/dist/notiflix-3.2.7.min.css';
 
 const refs = {
-    dataPicker: document.querySelector('#datetime-picker'),
+    datePicker: document.querySelector('#datetime-picker'),
     startBtm: document.querySelector('[data-start]'),
     days: document.querySelector('[data-days]'),
     hours: document.querySelector('[data-hours]'),
@@ -11,6 +12,22 @@ const refs = {
     seconds: document.querySelector('[data-seconds]')
 }
 
-console.log(refs.seconds)
+refs.startBtm.disabled = true;
+// refs.startBtm.addEventListener('click', onStartClick)
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0].getTime() <= new Date()) {
+            window.alert("Please choose a date in the future")
+            return;
+        }
+    },
+};
 
 
+
+flatpickr(refs.datePicker, options);
