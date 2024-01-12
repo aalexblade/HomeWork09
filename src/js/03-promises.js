fecea49034e3aeee0704e9da73dd7b3220b4916b
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-01-12 20:47:48 +0200

    task_03-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,18 +9,40 @@ const refs = {
 
 }
 
-refs.form.addEventListener('submit', onStart)
+refs.form.addEventListener('submit', onCreatePromises)
 
-function onStart(evt) {
+function onCreatePromises(evt) {
   evt.preventDefault()
 
+  const delay = Number(refs.delay.value)
+  const step = Number(refs.step.value)
+  const amoun = Number(refs.amoun.value)
+
+  for (let i = 1; i < amoun; i += 1) {
+    const specialDelay = Number(delay + step * (i - 1))
+    createPromise(i, specialDelay).then(onSuccess).catch(onError)
+  }
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  })
 }
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
